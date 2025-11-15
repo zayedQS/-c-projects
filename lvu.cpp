07a15171bb723a1ea2b9c60739f831de2f18f57b
zayedQS <zayedqs1@gmail.com>
2025-11-15 23:05:28 +0300

    Implement love response program in lvu.cpp

---
+++ lvu.cpp
@@ -0,0 +1,35 @@
+#include <iostream> 
+#include <string>
+
+using namespace std; 
+
+int main() {
+
+
+	
+
+
+	string name; 
+
+	cout << " do you love me ? : " << endl; 
+	cin >> name; 
+
+	while ( name !=  "yes") {
+		cout << " do you love me ? : " << endl;
+		cin >> name;
+
+	}
+
+
+
+	while (name == "yes") {
+		cout << "love you ^u^ ";
+
+	}
+
+
+
+
+
+	return 0;
+}
